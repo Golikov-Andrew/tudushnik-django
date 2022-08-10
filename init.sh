6b683ddf4fd49e67c2bdf7b59f191ae7be071a85
Alexander Ryzhov <a1exander@inbox.ru>
2022-08-10 13:53:31 +0300

    Change init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3 manage.py migrate && python3 manage.py collectstatic && python3 manage.py createsuperuser
+python3 manage.py migrate
+# && python3 manage.py collectstatic && python3 manage.py createsuperuser
