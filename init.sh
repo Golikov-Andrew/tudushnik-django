87e3aabe7f1f0935570d2c9dde1dacb248b27b6d
Alexander Ryzhov <a1exander@inbox.ru>
2022-08-25 12:56:43 +0300

    add init.sh to Dockerfile

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# User credentials
+user=admin
+email=admin@example.com
+password=pass
+
 python3 manage.py migrate
-# && python3 manage.py collectstatic && python3 manage.py createsuperuser
+python3 manage.py collectstatic
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
