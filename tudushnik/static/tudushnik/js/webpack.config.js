d74cae254bb076230ac5f7c560aeec6a8b132a20
Golikov-Andrew <andrejgolikov46@gmail.com>
2025-03-05 23:24:31 +0300

    add calendar class, view class

--- tudushnik/static/tudushnik/js/webpack.config.js
+++ tudushnik/static/tudushnik/js/webpack.config.js
@@ -5,7 +5,8 @@ const webpack = require('webpack');
 module.exports = {
     entry: {
         task: ['./src/task/index.js'],
-        viewport: ['./src/viewport/index.js']
+        viewport: ['./src/viewport/index.js'],
+        calendar: ['./src/calendar/index.js']
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -15,7 +16,8 @@ module.exports = {
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
-            moment: "moment"
+            moment: "moment",
+            moment_range: "moment-range"
         })
     ]
 };
