b5a45453dbde6edd32787a710049564d1403678a
Golikov-Andrew <andrejgolikov46@gmail.com>
2025-07-20 23:32:22 +0300

    2025-07-20_gav bugfix tasks update duration

--- tudushnik/static/tudushnik/js/duration_annotation.js
+++ tudushnik/static/tudushnik/js/duration_annotation.js
@@ -1,12 +1,13 @@
 const inp_id_duration = document.getElementById('id_duration')
 const parentElem = inp_id_duration.parentElement
 
-function createInputNumber(label_text, data_seconds) {
+function createInputNumber(label_text, data_seconds, max_value) {
     let elem = document.createElement('input')
     elem.classList.add('duration_controller')
     elem.setAttribute('type', 'number')
-    // elem.style.width = '42px'
     elem.setAttribute('min', '0')
+    if (max_value !== undefined)
+        elem.setAttribute('max', max_value);
     elem.setAttribute('data-seconds', data_seconds)
     let label = document.createElement('label')
     label.innerText = `${label_text}: `
@@ -17,17 +18,14 @@ function createInputNumber(label_text, data_seconds) {
 }
 
 const inp_duration_hours_ctrl = createInputNumber('Часов', 60 * 60)
-const inp_duration_minutes_ctrl = createInputNumber('Минут', 60)
-const inp_duration_seconds_ctrl = createInputNumber('Секунд', 1)
+const inp_duration_minutes_ctrl = createInputNumber('Минут', 60, 59)
+// const inp_duration_seconds_ctrl = createInputNumber('Секунд', 1, 59)
 
 
 let duration_controllers = document.getElementsByClassName('duration_controller')
 for (let i = 0, c; i < duration_controllers.length; i++) {
     c = duration_controllers[i]
-    c.addEventListener('change', (ev) => {
-        let new_val = +ev.target.value
-        calc_duration_in_sec(new_val)
-    })
+    c.addEventListener('change', calc_duration_in_sec)
 }
 
 
@@ -35,7 +33,7 @@ function calc_duration_in_sec() {
     let result = 0
     for (let i = 0, c; i < duration_controllers.length; i++) {
         c = duration_controllers[i]
-        result += +c.value * (+c.getAttribute('data-seconds'))
+        result += Math.floor((+c.value * (+c.getAttribute('data-seconds'))) / 60) * 60;
     }
     inp_id_duration.value = result
 }
@@ -45,10 +43,10 @@ init_duration_controllers(+inp_id_duration.value)
 function init_duration_controllers(new_val) {
     let hours = div(new_val, 60 * 60)
     let minutes = div(new_val - (hours * 60 * 60), 60)
-    let seconds = new_val - ((hours * 60 * 60) + (minutes * 60))
+    // let seconds = new_val - ((hours * 60 * 60) + (minutes * 60))
     inp_duration_hours_ctrl.value = hours
     inp_duration_minutes_ctrl.value = minutes
-    inp_duration_seconds_ctrl.value = seconds
+    // inp_duration_seconds_ctrl.value = seconds
 }
 
 const id_begin_at = document.querySelector('#id_begin_at')
