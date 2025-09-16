cf92a33aa8070ddbd35f8a82892f375889b530fe
Andrew_Golikov <andrejgolikov46@gmail.com>
2025-09-16 11:47:11 +0300

    edit User and Task serializers

--- tuduapi/serializers.py
+++ tuduapi/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from tudushnik.models.project import Project
@@ -5,11 +6,30 @@ from tudushnik.models.tag import Tag
 from tudushnik.models.task import Task
 
 
-class TaskSerializer(serializers.ModelSerializer):
+class DefaultValueSerializerMixin:
+    def get_fields(self):
+        fields = super().get_fields()
+        for field_name, field in fields.items():
+            model_field = self.Meta.model._meta.get_field(field_name)
+            if hasattr(model_field, 'default'):
+                field.default = model_field.default
+        return fields
+
+
+class TaskSerializer(DefaultValueSerializerMixin, serializers.ModelSerializer):
     class Meta:
         model = Task
         # fields = ['id', 'title', 'content', 'begin_at', 'duration', 'project_id']
+        read_only_fields = ('owner',)
         fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data['owner_id'] = self.context.get('request').user.pk
+        tags = validated_data.pop('tags')
+        task = Task.objects.create(**validated_data)
+        task.tags.set(tags)
+        task.save()
+        return task
 
 
 class ProjectSerializer(serializers.ModelSerializer):
